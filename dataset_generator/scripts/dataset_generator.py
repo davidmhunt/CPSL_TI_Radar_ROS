a200b0fd0fff056f1b1b7811afba5844e43a4639
happylifelsc <shaocheng.luo@gmail.com>
2023-11-29 14:40:44 -0500

    fixed dataset generator for faulty lidar data

--- dataset_generator/scripts/dataset_generator.py
+++ dataset_generator/scripts/dataset_generator.py
@@ -36,66 +36,77 @@ class DualTopicListener:
 
     def save_latest_radar_lidar(self,event):
         
-        self._save_latest_radar_data()
-        self._save_latest_lidar_data()
+        radar_success = self._save_latest_radar_data()
+        lidar_success = self._save_latest_lidar_data()
 
         #incrememt the sample index
-        self.sample_idx += 1
+        if radar_success and lidar_success:
+            self.sample_idx += 1
 
     def _save_latest_radar_data(self):
         #get the latest radar message
         msg = self.latest_data_radar
 
-        #get the real data
-        real_data = np.array(msg.real_data)
-        real_data = real_data.reshape(
-            (msg.layout.dim[0].size,
-            msg.layout.dim[1].size,
-            msg.layout.dim[2].size)
-        )
+        if msg is None:
+            return False
+        else:
 
-        #get the real data
-        imag_data = np.array(msg.imag_data)
-        imag_data = imag_data.reshape(
-            (msg.layout.dim[0].size,
-            msg.layout.dim[1].size,
-            msg.layout.dim[2].size)
-        )
+            #get the real data
+            real_data = np.array(msg.real_data)
+            real_data = real_data.reshape(
+                (msg.layout.dim[0].size,
+                msg.layout.dim[1].size,
+                msg.layout.dim[2].size)
+            )
 
-        data = real_data + 1j * imag_data
+            #get the real data
+            imag_data = np.array(msg.imag_data)
+            imag_data = imag_data.reshape(
+                (msg.layout.dim[0].size,
+                msg.layout.dim[1].size,
+                msg.layout.dim[2].size)
+            )
 
-        #save the radar data
-        file_name = "{}_{}.npy".format(self.save_file_name,self.sample_idx)
-        path = os.path.join(self.dataset_folder,self.radar_data_folder,file_name)
-        np.save(path,data)
+            data = real_data + 1j * imag_data
 
-        # log receiving the array
-        rx_channels = msg.layout.dim[0].size
-        samples_per_chirp = msg.layout.dim[1].size
-        chirps_per_frame = msg.layout.dim[2].size
-        sent_time = msg.header.stamp
+            #save the radar data
+            file_name = "{}_{}.npy".format(self.save_file_name,self.sample_idx)
+            path = os.path.join(self.dataset_folder,self.radar_data_folder,file_name)
+            np.save(path,data)
 
-        out_status = "Received ADCDataCube: rx channels: {}, samples: {}, chirps: {}, time: {}".format(rx_channels,samples_per_chirp,chirps_per_frame,sent_time)
-        rospy.loginfo(out_status)
+            # log receiving the array
+            rx_channels = msg.layout.dim[0].size
+            samples_per_chirp = msg.layout.dim[1].size
+            chirps_per_frame = msg.layout.dim[2].size
+            sent_time = msg.header.stamp
+
+            out_status = "Received ADCDataCube: rx channels: {}, samples: {}, chirps: {}, time: {}".format(rx_channels,samples_per_chirp,chirps_per_frame,sent_time)
+            rospy.loginfo(out_status)
+
+            return True
     
     def _save_latest_lidar_data(self):
         
         #get the latest lidar message
         msg = self.latest_data_lidar
-        sent_time = msg.header.stamp
-        #convert to a numpy array
-        gen = pc2.read_points(msg,skip_nans=True)
-        point_cloud = np.array(list(gen))
 
-        #save the pointcloud to a file
-        file_name = "{}_{}.npy".format(self.save_file_name,self.sample_idx)
-        path = os.path.join(self.dataset_folder,self.lidar_data_folder,file_name)
-        np.save(path,point_cloud)
+        if msg is None:
+            return False
+        else: 
+            sent_time = msg.header.stamp
+            #convert to a numpy array
+            gen = pc2.read_points(msg,skip_nans=True)
+            point_cloud = np.array(list(gen))
 
-        # log receiving the array
-        out_status = "Saved Lidar Point Cloud: points: {}, time: {}".format(np.shape(point_cloud)[0],sent_time)
-        rospy.loginfo(out_status)
-        pass
+            #save the pointcloud to a file
+            file_name = "{}_{}.npy".format(self.save_file_name,self.sample_idx)
+            path = os.path.join(self.dataset_folder,self.lidar_data_folder,file_name)
+            np.save(path,point_cloud)
+
+            # log receiving the array
+            out_status = "Saved Lidar Point Cloud: points: {}, time: {}".format(np.shape(point_cloud)[0],sent_time)
+            rospy.loginfo(out_status)
+            return True
 
     def timer_callback(self, event):
         if self.latest_data_lidar:
